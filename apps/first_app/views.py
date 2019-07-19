2433d02b077cf22f8b1630e4968a83ecea6609c2
Tommy Seo <tommyseo7m@gmail.com>
2019-07-18 19:36:19 -0700

    first commit

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from apps.first_app.models import *
+from django.contrib import messages
 
 # redirect localhost/8000 to localhost/8000/shows
 def redirect_url(request):
@@ -19,10 +20,17 @@ def create_show(request):
 
 # add button
 def add_show(request):
-    show = Show.objects.create(title=request.POST["show_add_title"], network=request.POST["show_add_network"],
-                            releaseDate=request.POST["show_add_date"], description=request.POST["show_add_description"])
+    errors = Show.objects.basic_validator(request.POST)
 
-    return redirect("/shows/" + str(show.id))
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect('/shows/new')
+    else:
+        show = Show.objects.create(title=request.POST["show_title"], network=request.POST["show_network"],
+                                releaseDate=request.POST["show_date"], description=request.POST["show_description"])
+        return redirect("/shows/" + str(show.id))
 
 # read one page
 def read_one_show(request, id):
@@ -35,15 +43,22 @@ def read_one_show(request, id):
 
 # update button
 def update_show(request, id):
-    show_id = int(id)
-    show = Show.objects.get(id=show_id)
-    show.title = request.POST["show_update_title"]
-    show.network = request.POST["show_update_network"]
-    show.releaseDate = request.POST["show_update_date"]
-    show.description = request.POST["show_update_description"]
-    show.save()
+    print(request.POST['show_network'])
+    errors = Show.objects.basic_validator(request)
 
-    return redirect("/shows/" + str(id))
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/" + str(id) + "/update")
+    else:
+        show_id = int(id)
+        show = Show.objects.get(id=show_id)
+        show.title = request.POST["show_title"]
+        show.network = request.POST["show_network"]
+        show.releaseDate = request.POST["show_date"]
+        show.description = request.POST["show_description"]
+        show.save()
+        return redirect("/shows/" + str(id))
 
 # update page
 def edit_show(request, id):
